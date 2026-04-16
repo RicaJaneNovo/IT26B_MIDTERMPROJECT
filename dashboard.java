05bda9d2efa99db358d03073afe5f57c624de289
RicaJaneNovo <ricajanenovo88@gmail.com>
2026-04-15 20:45:32 -0800

    Add files via upload
    final dashboard

--- dashboard.java
+++ dashboard.java
@@ -223,6 +223,8 @@ public class dashboard extends javax.swing.JFrame {
 }
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+            new settings(currentUser).setVisible(true);
+    this.dispose();
 
               
     }//GEN-LAST:event_jButton2ActionPerformed
